9478d341ec8f68e7326069c30e5e88d4c174ab00
NiceWin <saputrawindybintan@gmail.com>
2024-10-22 18:29:47 +0700

    Deploy-Database

--- config/db.js
+++ config/db.js
@@ -1,8 +1,19 @@
 const Sequelize = require("sequelize");
 
-const db = new Sequelize("myDB", "root", "secretpassword", {
-  host: "localhost",
-  dialect: "mysql",
-});
+// const db = new Sequelize("myDB", "root", "secretpassword", {
+//   host: "localhost",
+//   dialect: "mysql",
+// });
+
+const db = new Sequelize(
+  process.env.MYSQLDATABASE,
+  process.env.MYSQLUSER,
+  process.env.MYSQLPASSWORD,
+  {
+    host: process.env.MYSQLHOST,
+    port: process.env.MYSQLPORT,
+    dialect: "mysql",
+  }
+);
 
 module.exports = db;
